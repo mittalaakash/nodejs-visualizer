1a39f83900a8cddf6b233f8386beb1d218e68a66
Mridul <mit.ash.619@gmail.com>
2023-04-05 11:03:40 +0530

    frontend setup

---
+++ packages/frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Node.js Visualizer</h1>
+      <p>Welcome to the Node.js Visualizer!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
